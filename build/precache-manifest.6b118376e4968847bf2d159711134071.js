a02707c5bf98d66995a0847e36ecf6290c07c2cd
luyen <doduyluyen@gmail.com>
2020-02-20 16:51:13 +0700

    Commit to Test Header Get Request

---
+++ build/precache-manifest.6b118376e4968847bf2d159711134071.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5510a7cf91862832167576b6baab4082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff8c4d043958b5a0a26a",
+    "url": "/static/css/2.5fa6fcc4.chunk.css"
+  },
+  {
+    "revision": "14b17859da269394d853",
+    "url": "/static/css/main.9442386f.chunk.css"
+  },
+  {
+    "revision": "ff8c4d043958b5a0a26a",
+    "url": "/static/js/2.f95d5b1c.chunk.js"
+  },
+  {
+    "revision": "63390ead2b7852bc09136f16ae3cca59",
+    "url": "/static/js/2.f95d5b1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14b17859da269394d853",
+    "url": "/static/js/main.c614bc68.chunk.js"
+  },
+  {
+    "revision": "0fd4844c70cd856a0934",
+    "url": "/static/js/runtime-main.9951914a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89bcea4c5fe2b26f1ae887a0276260c6.js"
+  "/precache-manifest.6b118376e4968847bf2d159711134071.js"
 );
 
 self.addEventListener('message', (event) => {
